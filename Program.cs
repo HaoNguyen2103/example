e54357e12555d153cc52ff05b733c47bbbf7f7ae
HaoNguyen2103 <nguyenhao21031995@gmail.com>
2025-07-10 22:30:35 +0700

    add
    add

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 {
     public static void Main(string[] args)
     {
+        Console.WriteLine("Hay nhap 1 so tren ban phim ");
+        int so = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("so do la" + " " + so);
 
     }
 }
